2f2c3d6d64298673ce7c2a6c59fb3d6245c6c1d6
karine <karinebueno.365@gmail.com>
2022-12-02 20:29:47 +0400

    meu segundo commit

---
+++ java-anatomia-classes/src/edu/karine/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package edu.karine.primeirasemana;
+public class BoletimEstudantil {
+    // arquivo BoletimEstudantil.java
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+        }
+    }
+    
