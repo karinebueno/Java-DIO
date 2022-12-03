ae31f56cc5d28206a9fdce15e29de50db1f6891b
karine <karinebueno.365@gmail.com>
2022-12-03 10:20:12 +0400

    Atualizando a pasta de projeto operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,81 @@
+public class Operadores {
+   
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condicoes e verdadeira");
+        
+        }
+        System.out.println("fim");
+
+        //String nomeUm = "Karine";
+        //String nomeDois = "Karine";
+
+        //System.out.println(nomeUm == nomeDois);
+
+        //int numero1 = 1;
+        //int numero2 = 2;
+
+        //boolean simNao = numero1 == numero2;
+
+        //System.out.println("numeroUm e igual a numeroDois? " + simNao);
+
+        //simNao = numero1 != numero2;
+
+        //System.out.println("numeroUm e diferente do numeroDois? " + simNao);
+
+        //simNao = numero1 > numero2;
+
+        //System.out.println("numeroUm e maior que o numeroDois? " + simNao);
+
+        //if(numero1 > numero2)
+	        //System.out.print("Numero 1 maior que numero 2");
+
+        //if(numero1 < numero2)
+	        //System.out.print("Numero 1 e menor que numero 2");
+
+        //if(numero1 >= numero2)
+	        //System.out.print("Numero 1 maior ou igual que numero 2");
+
+        //if(numero1 <= numero2)
+	        //System.out.print("Numero 1 menor ou igual que numero 2");
+
+        //if(numero1 != numero2)
+	        //System.out.print("Numero 1 é diferente de numero 2");jav
+
+        //int a, b;
+
+        //a = 6;
+        //b = 6;
+        
+        //String resultado = a==b ?"verdadeiro" : "falso";
+
+        //if(a==b)
+        //    resultado = "verdadeiro";
+        //else 
+        //    resultado = "falso";
+        //System.out.println(resultado);
+
+        //boolean variavel = true;
+        //System.out.println(!variavel);
+        //System.out.println(variavel);
+        //variavel = !variavel;
+        //System.out.println(variavel);
+
+        //int numero = 5;
+        //x repeticao
+        //numero++;
+        //numero = numero + 1;
+        //System.out.println( ++ numero);
+        //System.out.println(numero --);
+        //System.out.println(numero);
+    
+    }
+}
