6218a652f06ddb9358f949809ea98fcae3be0d3e
karine <karinebueno.365@gmail.com>
2022-12-07 11:42:14 +0400

    comitando exercicio

---
+++ Estrutura de Repeticao em Java/src/Exercicio1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String NomeAluno;
+        int IdadeDoAluno;
+
+        while(true) {
+
+            System.out.println("Informe o nome do aluno: ");
+            NomeAluno = ler.next();
+            if (NomeDoAluno.equals("0")) break;
+            
+            System.out.println("Informe a idade do aluno :");
+            IdadeDoAluno = ler.nextInt();
+
+        }
+
+
+
+    }
+}
