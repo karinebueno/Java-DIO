84c2c1ad532ec860fcf0331b26885b267f8c77b5
karine <karinebueno.365@gmail.com>
2022-12-08 17:49:15 +0400

    Commitando exercicios

---
+++ Estrutura de Repeticao em Java/src/NumerosAleatorios.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class NumerosAleatorios {
+  
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.print("Numeros aleatorios: ");
+        for (int numero : numerosAleatorios){
+            System.out.print(numero + " ");
+        } 
+
+        System.out.print("\nSucessores ods Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios){
+            System.out.print((numero+1) + " ");
+        }
+        
+    }
+
+}
---
+++ Estrutura de Repeticao em Java/src/testando.java
@@ -0,0 +1,15 @@
+public class testando{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){   
+        int i;
+        int num =5;
+
+        for (i = 0; i < 3; i++){
+            num += i;
+            System.out.print( i + " " );
+        }
+    }
+}
